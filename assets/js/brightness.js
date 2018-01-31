79bb892e7ce175059fadde261f5cd6cfe61e1384
Ryan <ryanthults@tamu.edu>
2018-01-31 00:45:56 -0600

    Initial commit

---
+++ assets/js/brightness.js
@@ -0,0 +1,4 @@
+while(document.getElementById("slider").value != 100) {
+    document.getElementById("slider_icon").opacity = 0.0;
+    document.getElementById("slider_icon").filter = "alpha(opacity=50)";
+}
